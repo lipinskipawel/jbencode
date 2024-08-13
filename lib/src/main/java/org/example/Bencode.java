dfc03b5237895da9ed802d32e18a5624710dbbd5
Pawel Lipinski <lipinskijob@gmail.com>
2024-08-13 09:33:11 +0200

    Decode bencoded integers

--- lib/src/main/java/org/example/Bencode.java
+++ lib/src/main/java/org/example/Bencode.java
@@ -3,7 +3,22 @@ package org.example;
 public final class Bencode {
 
     public static String decode(String bencode) {
+        final var type = bencode.charAt(0);
+        if (type == 'i') {
+            return parseInteger(bencode);
+        }
         final var splitString = bencode.split(":", 2);
         return splitString[1];
+    }
+
+    private static String parseInteger(String bencode) {
+        final var number = bencode.substring(1, bencode.indexOf("e"));
+        if (number.charAt(0) == '0' && number.length() >= 2) {
+            throw new IllegalArgumentException("Bencode protocol format does not allows leading zeros like [%s]".formatted(number));
+        }
+        if (number.charAt(0) == '-' && number.charAt(1) == '0' && number.length() >= 3) {
+            throw new IllegalArgumentException("Bencode protocol format does not allows leading zeros like [%s]".formatted(number));
+        }
+        return number;
     }
 }
--- lib/src/test/java/org/example/BencodeTest.java
+++ lib/src/test/java/org/example/BencodeTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.example.Bencode.decode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BencodeTest implements WithAssertions {
@@ -23,8 +25,43 @@ class BencodeTest implements WithAssertions {
     @ParameterizedTest
     @MethodSource("strings")
     void decode_strings(String bencode, String expected) {
-        final var result = Bencode.decode(bencode);
+        final var result = decode(bencode);
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> integers() {
+        return Stream.of(
+                arguments("i3e", "3"),
+                arguments("i-3e", "-3"),
+                arguments("i0e", "0")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("integers")
+    void decode_integers(String bencode, String expected) {
+        final var result = decode(bencode);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> illegalIntegers() {
+        return Stream.of(
+                arguments("i03e", "03"),
+                arguments("i-03e", "-03"),
+                arguments("i-003e", "-003"),
+                arguments("i00038e", "00038")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("illegalIntegers")
+    void throw_when_illegally_encoded_integer(String bencode, String msg) {
+        final var result = Assertions.catchThrowable(() -> decode(bencode));
+
+        assertThat(result)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Bencode protocol format does not allows leading zeros like [%s]".formatted(msg));
     }
 }
